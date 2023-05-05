aaa5ee63bd2522799bc00795f8b1431e57f7ee57
Brian Takita <brian.takita@gmail.com>
2023-05-05 10:09:51 -0400

    fetch__template_pair_: fix: type

--- src/fetch__template_pair_/index.d.ts
+++ src/fetch__template_pair_/index.d.ts
@@ -1,4 +1,15 @@
 import type {response_pair_T} from '../response_pair_'
+export declare function fetch__template_pair_<
+	F extends ((...arg_a:any[])=>Promise<Response>)|any[],
+	T = unknown,
+>(
+	fetch_fn:
+		F extends (...arg_a:any[])=>Promise<Response>
+			? F
+			: F extends any[]
+				? (...arg_a:F)=>Promise<Response>
+				: never
+):fetch__template_pair_T<F, T>
 export type fetch__template_pair_T<
 	F extends ((...arg_a:any[])=>Promise<Response>)|any[],
 	T = unknown,
